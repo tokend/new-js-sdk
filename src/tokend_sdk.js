6f808c89a80186c62309757e358ede1a8986b246
ihordiachenko <ih.diachenko@gmail.com>
2018-05-07 16:25:38 +0300

    Add sdk facade

---
+++ src/tokend_sdk.js
@@ -0,0 +1,62 @@
+import { Api } from './api/api'
+import { Horizon } from './horizon/horizon'
+import { Wallet } from './wallet'
+
+/**
+ * TokendD Software Development Toolkit.
+ *
+ * @class
+ */
+export class TokenD {
+  /**
+   * Make a new TokenD SDK instance.
+   *
+   * @constructor
+   *
+   * @param {string} serverUrl TokenD backend url.
+   * @param {object} [opts]
+   * @param {boolean} [opts.allowHttp] Allow connecting to http servers, default: `false`. This must be set to false in production deployments!
+   * @param {Object} [opts.proxy] Proxy configuration. Look [axios docs](https://github.com/axios/axios#request-config) for more info
+   * @param {Object} [opts.httpBasicAuth] HTTP basic auth credentials. Look [axios docs](https://github.com/axios/axios#request-config) for more info.
+   * @param {Object} [opts.customHeaders] Custom headers for request.
+   * @param {boolean} [opts.withCredentials] Indicates whether or not cross-site Access-Control requests should be made using credentials.
+   */
+  constructor (serverUrl, opts) {
+    this._api = new Api(serverUrl, opts)
+    this._horizon = new Horizon(serverUrl, opts)
+  }
+
+  /**
+   * Horizon server instance.
+   */
+  get horizon () {
+    return this._horizon
+  }
+
+  /**
+   * API server instance.
+   */
+  get api () {
+    return this._api
+  }
+
+  /**
+   * User's wallet.
+   */
+  get wallet () {
+    return this._wallet
+  }
+
+  /**
+   * Use a wallet to sign transactions.
+   *
+   * @param {Wallet} wallet User's wallet.
+   */
+  useWallet (wallet) {
+    if (!(wallet instanceof Wallet)) {
+      throw new TypeError('A wallet instance expected.')
+    }
+
+    this._wallet = wallet
+  }
+}
---
+++ src/tokend_sdk.spec.js
@@ -0,0 +1,38 @@
+import { Horizon } from './horizon/horizon'
+import { Api } from './api/api'
+import { Wallet } from './wallet'
+import mocks from './test_helpers/mock_factory'
+
+import { TokenD } from './tokend_sdk'
+
+describe('TokenD', () => {
+  let sdk
+  const url = 'https://tokend.org/horizon'
+  const opts = { allowHttp: false }
+
+  beforeEach(() => {
+    sdk = new TokenD(url, opts)
+  })
+
+  describe('.constructor', () => {
+    it('Should make an SDK instance.', () => {
+      let sdk = new TokenD(url, opts)
+
+      expect(sdk).to.have.a.property('api').instanceOf(Api)
+      expect(sdk).to.have.a.property('horizon').instanceOf(Horizon)
+    })
+  })
+
+  describe('.useWallet', () => {
+    it('Should use a wallet.', () => {
+      let wallet = mocks.wallet()
+
+      expectNoThrow(() => sdk.useWallet(wallet))
+      expect(sdk).to.have.a.property('wallet').instanceOf(Wallet)
+    })
+
+    it('Should throw if an invalid wallet passed.', () => {
+      expectThrow(() => sdk.useWallet(null))
+    })
+  })
+})
