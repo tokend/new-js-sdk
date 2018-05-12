4b4587a97d4685f0efc34fda48955f0afd441f6b
ihordiachenko <ih.diachenko@gmail.com>
2018-05-12 15:48:57 +0300

    Adjust base error class to JSON-RPC standard

--- src/errors.js
+++ src/errors.js
@@ -28,6 +28,12 @@ export class TimeoutError extends Error {
  * Base class for server errors.
  */
 export class ServerErrorBase extends Error {
+  /**
+   * Wrap a raw axios error.
+   *
+   * @param {object} originalError Raw axios response.
+   * @param {axios} axios Axios instance used for request.
+   */
   constructor (originalError, axios) {
     super(originalError.message)
     this.originalError = originalError
@@ -39,6 +45,27 @@ export class ServerErrorBase extends Error {
    */
   get httpStatus () {
     return this.originalError.response.status
+  }
+
+  /**
+   * Error meta.
+   */
+  get meta () {
+    return this._meta
+  }
+
+  /**
+   * A short, human-readable summary of the problem.
+   */
+  get title () {
+    return this._title
+  }
+
+  /**
+   * A human-readable explanation specific to this occurrence of the problem.
+   */
+  get detail () {
+    return this._detail
   }
 
   /**
