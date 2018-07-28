8f28eca506168bdd90806fa709f0032305819f27
Vladislav <logvinenko.js@gmail.com>
2018-07-28 15:07:02 +0300

    Add and expose xdr enums

---
+++ src/const/enums.const.js
@@ -0,0 +1,47 @@
+import { xdr } from '../base'
+
+export function xdrEnumToConstant (xdrEnum) {
+  xdrEnum = typeof xdrEnum === 'string'
+    ? xdr[xdrEnum]
+    : xdrEnum
+
+  try {
+    const res = {}
+    xdrEnum.values().forEach(function (item) { res[item.name] = item.value })
+    return res
+  } catch (error) {
+    throw new Error(`xdrEnumToConstant: Cannot get values from provided xdrEnum (${xdrEnum})`)
+  }
+}
+
+export const REQUEST_TYPES = Object.freeze(
+  xdrEnumToConstant('ReviewableRequestType')
+)
+
+export const FEE_TYPES = Object.freeze(
+  xdrEnumToConstant('FeeType')
+)
+
+export const PAYMENT_FEE_SUBTYPES = Object.freeze(
+  xdrEnumToConstant('PaymentFeeType')
+)
+
+export const ASSET_POLICIES = Object.freeze(
+  xdrEnumToConstant('AssetPolicy')
+)
+
+export const ASSET_PAIR_POLICIES = Object.freeze(
+  xdrEnumToConstant('AssetPairPolicy')
+)
+
+export const ACCOUNT_TYPES = Object.freeze(
+  xdrEnumToConstant('AccountType')
+)
+
+export const SALE_STATE = Object.freeze(
+  xdrEnumToConstant('SaleState')
+)
+
+export const OP_TYPES = Object.freeze(
+  xdrEnumToConstant('OperationType')
+)
