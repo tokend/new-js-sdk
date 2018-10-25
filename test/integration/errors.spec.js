a4b8e8b54a537dc2653dd6d111d4b26c987395f6
Vladislav <logvinenko.js@gmail.com>
2018-10-25 18:58:50 +0300

    Update integration test

--- test/integration/errors.spec.js
+++ test/integration/errors.spec.js
@@ -1,26 +1,11 @@
 import { TokenD } from '../../src'
-import { NotFoundError } from '../../src/horizon/errors'
+import { NotFoundError } from '../../src/errors'
 
 const config = {
   HORIZON_URL: 'http://0.0.0.0:8001'
 }
 
 describe('errors integration test', () => {
-  it('should properly parse horizon error in legacy format', async () => {
-    const sdk = await TokenD.create(config.HORIZON_URL, {
-      allowHttp: true
-    })
-
-    const notExistingAssetCode = 'eutgweukrjwqrh'
-    const err = await catchPromise(sdk.horizon.assets.get(notExistingAssetCode))
-    console.log('LEGACY')
-    console.log(err.httpStatus)
-    console.log(err.meta)
-    console.log(err.title)
-    console.log(err.detail)
-    expect(err).to.be.an.instanceOf(NotFoundError)
-  })
-
   it('should properly parse horizon error in JSON API format', async () => {
     const sdk = await TokenD.create(config.HORIZON_URL, {
       allowHttp: true
@@ -28,11 +13,11 @@ describe('errors integration test', () => {
 
     const notExistingAssetCode = 'eutgweukrjwqrh'
     const err = await catchPromise(sdk.horizon.assets.get(notExistingAssetCode))
-    console.log('NOT LEGACY')
-    console.log(err.httpStatus)
-    console.log(err.meta)
-    console.log(err.title)
-    console.log(err.detail)
+
     expect(err).to.be.an.instanceOf(NotFoundError)
+    expect(err.httpStatus).to.not.equal(undefined)
+    expect(err.meta).to.not.equal(undefined)
+    expect(err.title).to.not.equal(undefined)
+    expect(err.detail).to.not.equal(undefined)
   })
 })
