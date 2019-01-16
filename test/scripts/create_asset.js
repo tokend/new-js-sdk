4dafaf5e43aae58b9870dc3887d023ecf81a8f5a
Vladislav <logvinenko.js@gmail.com>
2019-01-16 14:00:45 +0200

    Update scripts

--- test/scripts/create_asset.js
+++ test/scripts/create_asset.js
@@ -1,38 +1,100 @@
 import { Keypair } from '../../src/base'
 import { Asset } from '../helpers/asset'
 
-import { accountHelper } from '../helpers'
-import { assetHelper } from '../helpers'
-import { keyValueHelper } from '../helpers'
-import { requestHelper } from '../helpers'
+import { ASSET_POLICIES, KEY_VALUE_KEYS } from '../../src/const'
 
-import { KEY_VALUE_KEYS } from '../../src/const'
+import {
+  accountHelper, assetHelper, keyValueHelper, requestHelper
+} from '../helpers'
 
 /**
- * Creates a syndicate account, creates an asset and reviews it
- * @param opts
- * * @param {number} [opts.policies]
- *
+ * Creates a syndicate account, creates key-value an asset and reviews it.
+ * @param [opts] - see all possible opts: {@link Asset.create}
  */
-export async function createAsset (opts = {}) {
-  const ownerKp = Keypair.random()
-  const assetCode = Asset.randomCode()
+export async function createAssetFromScratch (opts = {}) {
+  console.log('createAssetFromScratch script execution started')
 
-  await Promise.all([
-    accountHelper.createSyndicate(ownerKp.accountId()),
-    keyValueHelper.putEntries({
-      [KEY_VALUE_KEYS.assetCreateTasks]: 1
-    })
-  ])
-  const requestId = await assetHelper.create({
-    policies: opts.policies,
-    code: assetCode,
-  }, ownerKp)
-  await requestHelper.approve(requestId)
+  const ownerKp = Keypair.random()
+  const code = Asset.randomCode()
+
+  console.log('Creating the syndicate account')
+  await accountHelper.createSyndicate(ownerKp.accountId())
+  console.log('Created the syndicate account')
+  // If asset code is provided through opts, the random code will be ignored
+  console.log('Creating and approving asset')
+  await createAndApproveAsset({ code, ...opts }, ownerKp)
+  console.log('Asset created and approved')
 
   return {
     ownerKp: ownerKp,
     ownerId: ownerKp.accountId(),
-    assetCode,
+    assetCode: code
   }
+}
+
+/**
+ * Creates the asset request and reviews it. Also will chech for k/v entry for
+ * asset create tasks and create one if entry doesn't exist
+ *
+ * @param {object} [opts] - see all possible opts: {@link Asset.create}
+ * * @param {object} [opts.code]
+ * * @param {object} [opts.policies]
+ * @param {Keypair} [ownerKp] - master will be owner if not provided
+ */
+export async function createAndApproveAsset (opts, ownerKp) {
+  console.log('createAndApproveAsset script execution started')
+
+  console.log('Trying to define tasks to remove')
+  let tasksToRemove = await keyValueHelper.getEntryValue(
+    KEY_VALUE_KEYS.assetCreateTasks
+  )
+
+  if (!tasksToRemove) {
+    console.log('Tasks to remove not set in the k/v storage')
+    tasksToRemove = 1
+    console.log('Creating k/v entry for tasks to remove, value: ' + tasksToRemove)
+    await keyValueHelper.putEntries({
+      [KEY_VALUE_KEYS.assetCreateTasks]: tasksToRemove
+    })
+  }
+  console.log('tasks to remove value defined, value: ' + tasksToRemove)
+
+  console.log('Creating the request')
+  const requestId = await assetHelper.create(opts, ownerKp)
+  console.log('Approving the request')
+  const response = await requestHelper.approve(requestId, { tasksToRemove })
+  console.log('Request approved, ending createAndApproveAsset script execution')
+
+  return response.data
+}
+
+/**
+ * Creates and returns the stats quote asset. If stats quote asset already
+ * exists, the script will just return it without creating the new one
+ */
+export async function createStatsQuoteAsset () {
+  console.log('createStatsQuoteAsset script execution started')
+  console.log('Checking for stats quote asset presence')
+  let asset = await assetHelper.loadStatsQuoteAsset()
+
+  if (asset) {
+    console.log('Stats quote asset exist: ' + asset.code)
+    console.log('ending createStatsQuoteAsset execution')
+    return asset
+  }
+
+  console.log('Stats quote asset doesn\'t exist')
+  const assetCode = Asset.randomCode('USD')
+  await createAndApproveAsset({
+    code: assetCode,
+    policies: ASSET_POLICIES.statsQuoteAsset
+  })
+  console.log('Created the stats quote asset: ' + assetCode)
+
+  console.log('Loading the stats quote asset')
+  asset = await assetHelper.mustLoad(assetCode)
+  console.log('Loaded the stats quote asset' + asset.code)
+
+  console.log('ending createStatsQuoteAsset execution')
+  return asset
 }
--- test/scripts/create_sale.js
+++ test/scripts/create_sale.js
@@ -1,11 +1,14 @@
-import { ASSET_POLICIES } from '../../src/const'
-import { assetHelper } from '../helpers'
-import { requestHelper } from '../helpers'
-import { saleHelper } from '../helpers'
-
+import { ASSET_POLICIES, KEY_VALUE_KEYS } from '../../src/const'
 import { Asset } from '../helpers/asset'
-
 import _times from 'lodash/times'
+
+import {
+  createAndApproveAsset,
+  createStatsQuoteAsset
+} from './create_asset'
+import {
+  sdk, assetPairHelper, requestHelper, saleHelper, keyValueHelper
+} from '../helpers'
 
 /**
  *
@@ -14,27 +17,83 @@ import _times from 'lodash/times'
  * @param {Keypair} ownerKp
  */
 export async function createSale (opts, ownerKp) {
-  const quoteAssetCodes = _times(Asset.randomCode(), 3)
-  const defaultQuoteAsset = Asset.randomCode('USD')
+  console.log('Create sale script execution started')
 
+  console.log('Creating the list of quote assets')
+  const quoteAssetCodes = _times(3, _ => Asset.randomCode())
   await Promise.all(
-    quoteAssetCodes.map(code => assetHelper.create({
+    quoteAssetCodes.map(quoteAssetCode => createAndApproveAsset({
       policies: ASSET_POLICIES.baseAsset,
-      code
+      code: quoteAssetCode
     }))
   )
-  await assetHelper.create({ code: defaultQuoteAsset })
+  console.log('Created list of quote assets')
 
+  console.log('Created the stats quote asset')
+  const statsQuoteAsset = await createStatsQuoteAsset()
+  console.log('Created stats quote asset')
+
+  console.log('Pairing quote assets with stats quote asset')
+  await Promise.all(
+    quoteAssetCodes.map(code => assetPairHelper.create({
+      base: statsQuoteAsset.code,
+      quote: code
+    }))
+  )
+  console.log('Paired quote assets with stats quote asset')
+
+  console.log('Creating default quote asset for our sale')
+  const defaultQuoteAsset = Asset.randomCode('USD')
+  await createAndApproveAsset({
+    code: defaultQuoteAsset
+  })
+  console.log('Created default quote asset for our sale')
+
+  console.log('Pairing quote assets with default quote asset')
+  await Promise.all(
+    quoteAssetCodes.map(code => assetPairHelper.create({
+      base: defaultQuoteAsset,
+      quote: code
+    }))
+  )
+  console.log('Paired quote assets with default quote asset')
+
+  console.log('Lading base asset to define max amount to be sold')
+  const { data: baseAssetData } = await sdk.horizon.assets.get(opts.baseAsset)
+  const maxAmountToBeSold = baseAssetData.maxIssuanceAmount
+  console.log('Loaded base asset, max amount to be sold: ' + maxAmountToBeSold)
+
+  console.log('Trying to define tasks to remove')
+  let tasksToRemove = await keyValueHelper.getEntryValue(
+    KEY_VALUE_KEYS.saleCreateTasks
+  )
+
+  if (!tasksToRemove) {
+    console.log('Tasks to remove not set in the k/v storage')
+    tasksToRemove = 1
+    console.log('Creating k/v entry for tasks to remove, value: ' + tasksToRemove)
+    await keyValueHelper.putEntries({
+      [KEY_VALUE_KEYS.saleCreateTasks]: tasksToRemove
+    })
+  }
+  console.log('tasks to remove value defined, value: ' + tasksToRemove)
+
+  console.log('Creating the sale itself')
   const requestId = await saleHelper.create({
     defaultQuoteAsset,
+    baseAssetForHardCap: maxAmountToBeSold,
     baseAsset: opts.baseAsset,
-    quoteAssets: quoteAssetCodes.map(asset => ( {price: '1', asset} ))
+    quoteAssets: quoteAssetCodes.map(asset => ({ price: '1', asset }))
   }, ownerKp)
-  await requestHelper.approve(requestId)
+  console.log('Created the sale creation request')
 
-  const { data: sale } = await saleHelper.mustLoadByQuery({
-    base_asset: opts.baseAsset
-  })
+  console.log('Approving the sale creation request')
+  await requestHelper.approve(requestId, { tasksToRemove })
+  console.log('Approved the sale creation request')
+
+  console.log('Loading newly created sale')
+  const sale = await saleHelper.mustLoadByBaseAsset(opts.baseAsset)
+  console.log('Loaded newly created sale, returning')
 
   return {
     sale,
