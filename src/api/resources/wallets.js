bc359d5a41136476d5320a60370f8bee039b5f06
ardier16 <ardier16@gmail.com>
2019-02-14 12:13:41 +0200

    Fix changePassword method
    + use newMainWallet.keypair.accountId() instead of newEncryptedWallet.accountId

--- src/api/resources/wallets.js
+++ src/api/resources/wallets.js
@@ -293,7 +293,7 @@ export class Wallets extends ResourceGroupBase {
           id: encryptedNewMainWallet.id,
           attributes: {
             email: oldWallet.email,
-            accountId: encryptedNewMainWallet.accountId,
+            accountId: newMainWallet.keypair.accountId(),
             salt: encryptedNewMainWallet.salt,
             keychainData: encryptedNewMainWallet.keychainData
           },
