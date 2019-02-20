725d4f881989c3e23ced152dc7210cb62a524c30
Hennadii <hennadii.ponedielnikov@gmail.com>
2019-02-20 15:52:49 +0200

    updated changelog (again)

--- src/base/operations/review_request_builder.js
+++ src/base/operations/review_request_builder.js
@@ -104,7 +104,7 @@ export class ReviewRequestBuilder {
    * @param {string} opts.requestHash - Hash of the request to be reviewed
    * @param {number} opts.action - action to be performed over request (xdr.ReviewRequestOpAction)
    * @param {string} opts.reason - Reject reason
-   * @param {string} opts.request
+   * @param {string} opts.requestDetails - external details of the withdrawal request
    * @param {string} [opts.source] - The source account for the payment. Defaults to the transaction's source account.
    * @param {object} opts.reviewDetails
    * @param {number|string} opts.reviewDetails.tasksToAdd - new tasks for reviewable request to be accomplished before fulfill
