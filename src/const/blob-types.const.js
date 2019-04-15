5361b45b0d6379d674ce1cb00d3a3055c6709975
ardier16 <ardier16@gmail.com>
2019-04-15 12:07:07 +0300

    Fix issues
    + rename kycSyndicate -> kycCorporate
    - unnecessary types

--- src/const/blob-types.const.js
+++ src/const/blob-types.const.js
@@ -1,10 +1,8 @@
 export const BLOB_TYPES = Object.freeze({
   assetDescription: 'asset_description',
   fundOverview: 'fund_overview',
-  fundUpdate: 'fund_update',
-  navUpdate: 'nav_update',
   fundDocument: 'fund_document',
-  kycSyndicate: 'alpha',
+  kycCorporate: 'alpha',
   kycGeneral: 'kyc_form',
   bravo: 'bravo'
 })
