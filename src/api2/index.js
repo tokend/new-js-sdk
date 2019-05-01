d45369e0b803cca8ed4001e810c058095aa0f00a
ardier16 <ardier16@gmail.com>
2019-05-01 14:30:54 +0300

    Add missing signers manager export

--- src/api2/index.js
+++ src/api2/index.js
@@ -1,3 +1,3 @@
 export { ApiCaller } from './api-caller'
-export { WalletsManager, FactorsManager } from './managers'
+export { WalletsManager, FactorsManager, SignersManager } from './managers'
 export { JsonapiResponse } from './middlewares/parse-jsonapi-response'
