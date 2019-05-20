5ac44a69ee8bbb7c55f9bbb4e4da2582c33759dd
Yaroslav Panasenko <yar.panasenko@gmail.com>
2019-05-20 17:22:52 +0300

    Added integration tests for change role request

---
+++ test/helpers/change_role.js
@@ -0,0 +1,44 @@
+import { getRequestIdFromResultXdr, Helper } from './_helper'
+import { base } from '../../src'
+
+export class ChangeRole extends Helper {
+  /**
+   * Creates operation to create KYC request
+   * @param {object} opts
+   * @param {number|string} opts.requestID - set to zero to create new request
+   * @param {string} opts.destinationAccount
+   * @param {string} opts.accountRoleToSet
+   * @param {object} opts.creatorDetails
+   * @param {number|string} opts.allTasks
+   * @param {string} opts.creatorDetails - request details set by creator
+   * @param {string} [opts.source] - The source account for the payment. Defaults to the transaction's source account.
+   * @param {Keypair} ownerKp
+   *
+   * @returns {string} the ID of the request
+   */
+  async create (opts, ownerKp = this.masterKp) {
+    const DEFAULTS = {
+      requestID: '0',
+      creatorDetails: { 'test': 'test' }
+    }
+
+    const operation = base.CreateChangeRoleRequestBuilder.createChangeRoleRequest({
+      ...DEFAULTS,
+      ...opts
+    })
+
+    const response = await this.submit(operation, ownerKp)
+
+    return getRequestIdFromResultXdr(response.resultXdr, 'createChangeRoleRequestResult')
+  }
+
+  async cancel (reqId, ownerKp = this.masterKp) {
+    const operation = base.CancelChangeRoleRequestBuilder.cancelChangeRoleRequest({
+      requestID: reqId
+    })
+
+    const response = await this.submit(operation, ownerKp)
+
+    return getRequestIdFromResultXdr(response.resultXdr, 'cancelChangeRoleRequestResult')
+  }
+}
---
+++ test/integration/change_role.spec.js
@@ -0,0 +1,7 @@
+
+
+describe('Change role request', () => {
+  it('should create and cancel change role request', async () => {
+    
+  })
+})
