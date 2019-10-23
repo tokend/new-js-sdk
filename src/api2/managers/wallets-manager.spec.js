fc14ae41aebcef7c83eda352f5998f1b4276a484
Roman Malyshev <lem0000000000n@gmail.com>
2019-10-23 15:36:50 +0300

    Add create wallet with signers and integration test for create and recovery wallet

--- src/api2/managers/wallets-manager.spec.js
+++ src/api2/managers/wallets-manager.spec.js
@@ -17,17 +17,25 @@ describe('Wallets manager', () => {
       ApiCaller.getInstance('https://api.test.com')
     )
 
-    sandbox.stub(walletsManagerInstance._apiCaller, 'get')
-      .withArgs('/kdf')
+    let stubGet = sandbox.stub(walletsManagerInstance._apiCaller, 'get')
+      .withArgs('/v3/key_values/signer_role:default')
       .resolves({
         data: {
-          bits: 256,
-          n: 8,
-          p: 1,
-          r: 8,
-          salt: '/1dwsCq6f1zdpIObxLBOiQ=='
+          value: {
+            u32: 1
+          }
         }
       })
+
+    stubGet.withArgs('/kdf').resolves({
+      data: {
+        bits: 256,
+        n: 8,
+        p: 1,
+        r: 8,
+        salt: '/1dwsCq6f1zdpIObxLBOiQ=='
+      }
+    })
   })
 
   afterEach(() => {
@@ -51,6 +59,23 @@ describe('Wallets manager', () => {
             p: 1,
             r: 8,
             salt: '/1dwsCq6f1zdpIObxLBOiQ=='
+          }
+        })
+      })
+    })
+
+    describe('getSignerRoleId', () => {
+      it('returns signer role id calling _apiCaller.get with correct params', async () => {
+        const result = await walletsManagerInstance
+          .getSignerRoleId()
+
+        expect(walletsManagerInstance._apiCaller.get)
+          .to.have.been.calledOnceWith('/v3/key_values/signer_role:default')
+        expect(result).to.deep.equal({
+          data: {
+            value: {
+              u32: 1
+            }
           }
         })
       })
@@ -111,6 +136,49 @@ describe('Wallets manager', () => {
             'some-session-key'
           )
         )
+      })
+    })
+
+    describe('createWithSigners', () => {
+      it('returns new wallet, created by calling _apiCaller.post method with generated params', async () => {
+        sandbox.stub(Wallet, 'generate').returns(
+          new Wallet(
+            'foo@bar.com',
+            'SBLSDQ764O5IDRAFZXQJMBAJXWL3Z73SATJTAOIPGINPPUZ67E5VKIB3',
+            'GBUQDWXPPEFREJPI45CUPACMY6AQINP4DQ2DFXAF6YISPF3C4FFJ3U5S',
+            'some-wallet-id'
+          )
+        )
+        sandbox.stub(walletsManagerInstance._apiCaller, 'post')
+          .withArgs('/wallets')
+          .resolves({
+            data: {
+              session: {
+                id: 'some-session-id',
+                encryptionKey: 'some-session-key'
+              }
+            }
+          })
+        const result = await walletsManagerInstance.createWithSigners(
+          'foo@bar.com',
+          'qwe123',
+          []
+        )
+
+        expect(Wallet.generate).to.have.been.calledWithExactly('foo@bar.com')
+        expect(walletsManagerInstance._apiCaller.post)
+          .to.have.been.calledOnceWith('/wallets')
+
+        expect(result.response).to.deep.equal({
+          data: {
+            session: {
+              id: 'some-session-id',
+              encryptionKey: 'some-session-key'
+            }
+          }
+        })
+        expect(result.wallet.email).to.equal('foo@bar.com')
+        expect(result.wallet.accountId).to.equal('GBUQDWXPPEFREJPI45CUPACMY6AQINP4DQ2DFXAF6YISPF3C4FFJ3U5S')
       })
     })
 
@@ -213,8 +281,8 @@ describe('Wallets manager', () => {
             'some-wallet-id'
           )
         )
-        sandbox.stub(walletsManagerInstance, '_getAccountIdByRecoveryId')
-          .resolves('SOME_ACCOUNT_ID')
+        sandbox.stub(walletsManagerInstance, '_getAccountIdByEmail')
+          .resolves('GBUQDWXPPEFREJPI45CUPACMY6AQINP4DQ2DFXAF6YISPF3C4FFJ3U5S')
 
         sandbox.stub(
           walletsManagerInstance._signersManager,
@@ -229,8 +297,11 @@ describe('Wallets manager', () => {
           'qwe123'
         )
 
-        expect(Wallet.generate).to.have.been.calledWithExactly('foo@bar.com')
-        expect(walletsManagerInstance._getAccountIdByRecoveryId)
+        expect(Wallet.generate).to.have.been.calledWithExactly(
+          'foo@bar.com',
+          'GBUQDWXPPEFREJPI45CUPACMY6AQINP4DQ2DFXAF6YISPF3C4FFJ3U5S'
+        )
+        expect(walletsManagerInstance._getAccountIdByEmail)
           .to.have.been.calledOnce
         expect(walletsManagerInstance._signersManager
           .createChangeSignerTransaction
