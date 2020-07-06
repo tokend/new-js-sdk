2974bd3a9d283223a6dbf0bf0b3cfa923e1ffb1f
ArtSuslov <artursuslov@gmail.com>
2020-07-06 15:06:33 +0300

    implemented document class and added upload documents feature

---
+++ src/base/document.js
@@ -0,0 +1,104 @@
+import { documentsManager } from '@/api'
+
+/**
+ * Wrapper to simplify work with documents
+ * @class
+ */
+export class Document {
+  /**
+   * @param {object} opts
+   * @param {File} [opts.file] - file representing a document
+   * @param {string} [opts.name] - file name
+   * @param {string} [opts.mimeType] - file MIME type
+   * @param {string} [opts.key] - storage service file key
+   * @param {string} [opts.docType] - storage service document type
+   */
+  constructor (opts = {}) {
+    this._file = opts.file || null
+    this._name = opts.name || ''
+    this._mimeType = opts.mimeType || ''
+    this._key = opts.key || ''
+    this._docType = opts.docType || ''
+    this._privateUrl = ''
+  }
+
+  get file () { return this._file }
+  get name () { return this._name }
+  get mimeType () { return this._mimeType }
+  get key () { return this._key }
+  get docType () { return this._docType }
+  get privateUrl () { return this._privateUrl }
+
+  get publicUrl () {
+    return documentsManager.getDocumentUrlByKey(this._key)
+  }
+
+  get isEmpty () {
+    return !this._file && !this.isUploaded
+  }
+
+  get isUploaded () {
+    return Boolean(this._key)
+  }
+
+  setFile (val) {
+    this._file = val
+    return this
+  }
+
+  setName (val) {
+    this._name = val
+    return this
+  }
+
+  setMimeType (val) {
+    this._mimeType = val
+    return this
+  }
+
+  setKey (val) {
+    this._key = val
+    return this
+  }
+
+  setDocType (val) {
+    this._docType = val
+    return this
+  }
+
+  toJSON () {
+    return {
+      mime_type: this._mimeType || '',
+      name: this._name || '',
+      key: this._key || ''
+    }
+  }
+
+  async uploadSelf () {
+    if (this.isUploaded) return this
+    if (this.isEmpty) throw new Error('Cannot upload an empty doc')
+    const opts = {
+      type: this._docType,
+      mimeType: this._mimeType,
+      file: this._file
+    }
+    this._key = await documentsManager.uploadDocument(opts)
+    return this
+  }
+
+  async getPrivateUrl () {
+    if (!this.isUploaded) return ''
+    this._privateUrl = await documentsManager.getPrivateUrl(this._key)
+    return this
+  }
+
+  static fromObj (obj) {
+    if (!this.isDoc(obj)) return null
+    return new Document(obj)
+  }
+
+  static isDoc (obj) {
+    if (!(obj && typeof obj === 'object')) return false
+    return !this.isEmpty
+  }
+}
---
+++ src/base/util/upload-documents.js
@@ -0,0 +1,44 @@
+import { Document } from '../document'
+import isObject from 'lodash/isObject'
+
+/**
+ * Uploads an array of documents to the storage.
+ *
+ * @param {Document[]} documents - array of {@link Document}
+ * instances to be uploaded
+ * @param {string} [accountId] - documents' owner account ID
+ */
+export async function uploadDocuments (documents) {
+  await Promise.all(documents.map(doc => uploadDocument(doc)))
+}
+
+export async function uploadDocumentsDeep (obj) {
+  const docs = collectDocsToUploadDeep(obj)
+  await uploadDocuments(docs)
+}
+
+/**
+ * Uploads a document to the storage.
+ *
+ * @param {Document} [document] - instance of {@link Document}
+ * to be uploaded
+ * @returns {Promise} Modified document with set key
+ */
+async function uploadDocument (document) {
+  if (!(document instanceof Document)) return
+  return document.uploadSelf()
+}
+
+function collectDocsToUploadDeep (obj = {}) {
+  const docs = []
+  for (const val of Object.values(obj)) {
+    if (val instanceof Document && !val.isUploaded) {
+      docs.push(val)
+      continue
+    }
+    if (isObject(val)) {
+      docs.push(...collectDocsToUploadDeep(val))
+    }
+  }
+  return docs
+}
