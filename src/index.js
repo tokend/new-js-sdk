b39348909778f31463907595f54baba60a9d2b1c
ArtSuslov <artursuslov@gmail.com>
2020-07-13 14:49:17 +0300

    review fixes

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import * as commonErrors from './errors'
 export * from './tokend_sdk'
 export * from './const'
 export { Document, uploadDocuments, uploadDocumentsDeep } from './utils/document'
-// export { uploadDocuments, uploadDocumentsDeep } from './util/upload-documents'
 export { default as base } from './base'
 export { Wallet, encryptSecretSeed, decryptSecretSeed } from './wallet'
 
