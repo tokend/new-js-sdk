e488680de49a3d7ec3246e1b83e8fe61ff98eb0c
Yaroslav Panasenko <yar.panasenko@gmail.com>
2020-11-23 15:09:29 +0200

    Update csv parser tests

--- test/integration/csv_parser.spec.js
+++ test/integration/csv_parser.spec.js
@@ -5,13 +5,13 @@ const performanceTestsRowsCount = 1_000_000
 describe('CSV Parser tests', () => {
   describe('Integration tests', () => {
     it('Upload valid "* .csv" file for the Mass Compulsory Asset Transfer operation', async () => {
-      const csv = "Operation type,Asset,Amount,Source,Destination,Creator details\nCompulsory Asset Transfer,BTC,10,GBA4EX43M25UPV4WIE6RRMQOFTWXZZRIPFAI5VPY6Z2ZVVXVWZ6NEOOB,GDAH3TH2AXUHU5EMWIUHTILN33A52OR7U6H7NGCLYJNYEPDZIKHD5WZD,{\"testKey\": \"testValue\"}"
+      const csv = "Operation type,Asset,Amount,Source,Source type,Destination,Destination type,Creator details\nCompulsory Asset Transfer,BTC,10,GBA4EX43M25UPV4WIE6RRMQOFTWXZZRIPFAI5VPY6Z2ZVVXVWZ6NEOOB,account_id,GDAH3TH2AXUHU5EMWIUHTILN33A52OR7U6H7NGCLYJNYEPDZIKHD5WZD,account_id,{\"testKey\": \"testValue\"}"
 
       await expectPromiseNoThrow(csvParserHelper.uploadCsv(csv,
         `/integrations/csv-parser/files/${masterKP.accountId()}/asset-transfers`))
     })
     it('Upload invalid "* .csv" file for the Mass Compulsory Asset Transfer operation', async () => {
-      const csv = "Operation type,Asset,Amount,Source,Destination,Creator details\nCompulsory Asset Transfer,BTC,10,GBA4EX43M25UPV4WIE6RRMQOFTWXZZRIPFAI5VPY6Z2ZVVXVWZ6NEOOB,,{\"testKey\": \"testValue\"}"
+      const csv = "Operation type,Asset,Amount,Source,Source type,Destination,Destination type,Creator details\nCompulsory Asset Transfer,BTC,10,GBA4EX43M25UPV4WIE6RRMQOFTWXZZRIPFAI5VPY6Z2ZVVXVWZ6NEOOB,account_id,,,{\"testKey\": \"testValue\"}"
 
       await catchPromise(csvParserHelper.uploadCsv(csv,
         `/integrations/csv-parser/files/${masterKP.accountId()}/asset-transfers`))
@@ -29,13 +29,13 @@ describe('CSV Parser tests', () => {
         `/integrations/csv-parser/files/${masterKP.accountId()}/reviews`))
     })
     it('Upload valid "* .csv" file for the Mass Unlock of Unclaimed Assets operation', async () => {
-      const csv = "Operation type,Deferred Payment,Asset,Amount,Source,Creator details\nUnlock of Unclaimed Assets,1,UAH,10,GBA4EX43M25UPV4WIE6RRMQOFTWXZZRIPFAI5VPY6Z2ZVVXVWZ6NEOOB,{\"testKey\": \"testValue\"}"
+      const csv = "Operation type,Deferred Payment,Asset,Amount,Creator details\nUnlock of Unclaimed Assets,1,UAH,10,{\"testKey\": \"testValue\"}"
 
       await expectPromiseNoThrow(csvParserHelper.uploadCsv(csv,
         `/integrations/csv-parser/files/${masterKP.accountId()}/asset-unlocks`))
     })
     it('Upload invalid "* .csv" file for the Mass Unlock of Unclaimed Assets operation', async () => {
-      const csv = "Operation type,Deferred Payment,Asset,Amount,Source,Creator details\nUnlock of Unclaimed Assets,1,UAH,,GBA4EX43M25UPV4WIE6RRMQOFTWXZZRIPFAI5VPY6Z2ZVVXVWZ6NEOOB,{\"testKey\": \"testValue\"}"
+      const csv = "Operation type,Deferred Payment,Asset,Amount,Creator details\nUnlock of Unclaimed Assets,1,UAH,,{\"testKey\": \"testValue\"}"
 
       await catchPromise(csvParserHelper.uploadCsv(csv,
         `/integrations/csv-parser/files/${masterKP.accountId()}/asset-unlocks`))
@@ -55,9 +55,9 @@ describe('CSV Parser tests', () => {
   })
   describe('Performance tests', () => {
     it('Upload the "* .csv" file with the data for 1 million transactions for the Mass Compulsory Asset Transfer operation', async () => {
-      var csv = "Operation type,Asset,Amount,Source,Destination,Creator details"
+      var csv = "Operation type,Asset,Amount,Source,Source type,Destination,Destination type,Creator details"
       for (var i = 0; i < performanceTestsRowsCount; i++) {
-        csv = csv + "\nCompulsory Asset Transfer,BTC,10,GBA4EX43M25UPV4WIE6RRMQOFTWXZZRIPFAI5VPY6Z2ZVVXVWZ6NEOOB,GDAH3TH2AXUHU5EMWIUHTILN33A52OR7U6H7NGCLYJNYEPDZIKHD5WZD,{\"testKey\": \"testValue\"}"
+        csv = csv + "\nCompulsory Asset Transfer,BTC,10,GBA4EX43M25UPV4WIE6RRMQOFTWXZZRIPFAI5VPY6Z2ZVVXVWZ6NEOOB,account_id,GDAH3TH2AXUHU5EMWIUHTILN33A52OR7U6H7NGCLYJNYEPDZIKHD5WZD,account_id,{\"testKey\": \"testValue\"}"
       }
 
       await expectPromiseNoThrow(csvParserHelper.uploadCsv(csv,
@@ -73,9 +73,9 @@ describe('CSV Parser tests', () => {
         `/integrations/csv-parser/files/${masterKP.accountId()}/reviews`))
     })
     it('Upload the "* .csv" file with the data for 1 million transactions for the Mass Unlock of Unclaimed Assets operation', async () => {
-      var csv = "Operation type,Deferred Payment,Asset,Amount,Source,Creator details"
+      var csv = "Operation type,Deferred Payment,Asset,Amount,Creator details"
       for (var i = 0; i < performanceTestsRowsCount; i++) {
-        csv = csv + "\nUnlock of Unclaimed Assets,1,UAH,10,GBA4EX43M25UPV4WIE6RRMQOFTWXZZRIPFAI5VPY6Z2ZVVXVWZ6NEOOB,{\"testKey\": \"testValue\"}"
+        csv = csv + "\nUnlock of Unclaimed Assets,1,UAH,10,{\"testKey\": \"testValue\"}"
       }
 
       await expectPromiseNoThrow(csvParserHelper.uploadCsv(csv,
