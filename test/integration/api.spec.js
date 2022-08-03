78e0a28dccef455cf8c0c75cee384682b81b4b06
Skriabin <artem.skriabin@distributedlab.com>
2022-08-03 13:47:14 +0300

    Add delete identity test

--- test/integration/api.spec.js
+++ test/integration/api.spec.js
@@ -1,5 +1,5 @@
 import { NotFoundError } from '../../src/errors'
-import { sdk } from '../helpers'
+import { sdk, api } from '../helpers'
 
 describe('API integration test', () => {
   describe('errors', () => {
@@ -12,6 +12,14 @@ describe('API integration test', () => {
       expect(err.meta).to.exist
       expect(err.title).to.exist
       expect(err.detail).to.exist
+    })
+    it('should properly parse api error from delete identity endpoint', async () => {
+      
+      let err = await catchPromise(api.deleteWithSignature('/identities/undefinedqmfnapnsk@mail.com'))
+    
+      expect(err).to.be.an.instanceOf(NotFoundError)
+      expect(err.httpStatus).to.exist
+      expect(err.title).to.exist
     })
   })
 })
